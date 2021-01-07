12aa7c43df751a3b5314e7eafbbaf67aa260fed0
Maria Solano <majosolano99@gmail.com>
2021-01-07 11:37:34 -0500

    First commit

---
+++ Assets/Scripts/Hilbert.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hilbert : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] float walkingSpeed = 1f;
+
+    private Rigidbody2D rigidBody = default;
+    private Animator animator = default;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Walk();
+        FlipSprite();
+    }
+
+    private bool IsPlayerMovingHorizontally() 
+    {
+        return Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon;
+    }
+
+    private void Walk()
+    {
+        float controlInput = Input.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlInput * walkingSpeed, rigidBody.velocity.y);
+        rigidBody.velocity = playerVelocity;
+
+        animator.SetBool("isWalking", IsPlayerMovingHorizontally());
+    }
+
+    private void FlipSprite()
+    {
+        if( IsPlayerMovingHorizontally() )
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rigidBody.velocity.x), 1f);
+        }
+    }
+}
---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private GameController gameController = default;
+    private void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if( collider.gameObject.name == "Hilbert" )
+        {
+            gameController.LoadGame();
+        }
+    }
+}
